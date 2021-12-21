628ec5a7461feabc44e3ca06732f158afa23ffb4
Kanchan <kanchanpandit15@gmail.com>
2021-12-21 17:26:35 +0530

    parameterized wait

--- 18-Automation/src/main/pages/waitutility/Waitutility.java
+++ 18-Automation/src/main/pages/waitutility/Waitutility.java
@@ -7,10 +7,12 @@ import baselibrary.Baselibrary;
 
 public class Waitutility extends Baselibrary
 {
-	public void alertIsPresent()
+	public void alertIsPresent(int time)
 	{
-		WebDriverWait wait = new WebDriverWait(driver,20);
+		WebDriverWait wait = new WebDriverWait(driver,time);
 		wait.until(ExpectedConditions.alertIsPresent());
 	}
+	
+	
 
 }
