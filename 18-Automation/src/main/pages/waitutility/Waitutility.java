84087351a58ee6268b548a753d766f201bd1d94c
Kanchan <kanchanpandit15@gmail.com>
2021-12-21 17:14:29 +0530

    Wait Alert is present done

--- 18-Automation/src/main/pages/waitutility/Waitutility.java
+++ 18-Automation/src/main/pages/waitutility/Waitutility.java
@@ -1,7 +1,16 @@
 package waitutility;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import baselibrary.Baselibrary;
 
-public class Waitutility extends Baselibrary {
+public class Waitutility extends Baselibrary
+{
+	public void alertIsPresent()
+	{
+		WebDriverWait wait = new WebDriver(driver,20);
+		wait.until(ExpectedConditions.alertIsPresent());
+	}
 
 }
